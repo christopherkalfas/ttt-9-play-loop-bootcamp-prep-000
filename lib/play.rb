db785575c057f20bf69259f2330c8af28bc43063
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-06 23:49:41 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  turn(board) = 0
-  while turn(board) < 9 
+  counter = 0
+  while counter < 9 
     turn(board)
-    turn(board) += 1
+    counter += 1
   end
 end
